82773509f564b4c1cf36bd2365042aac28127ef8
Razial <uck15@bk.ru>
2023-03-19 13:04:20 +0300

    Send an email with titles, descriptions and source site name

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import requests
 import os
+from send_email import send_email
 
 API_KEY = os.getenv('Api_key')
-url = f'https://newsapi.org/v2/everything?q=russia&from=2023-02-19&' \
+url = f'https://newsapi.org/v2/everything?q=russia&' \
+      f'from=2023-02-19&language=ru&' \
       f'sortBy=publishedAt&apiKey={API_KEY}'
 
 # Make request
@@ -10,7 +12,12 @@ request = requests.get(url)
 
 # Get a dictionary with data
 content = request.json()
+message = ''
 
 # Access the article titles and description
 for article in content['articles']:
-      print(article['title'])
+    message += article['title'] + '\n'
+    message += article['description'] + '\n'
+    message += article['source']['name'] + 2*'\n'
+
+send_email(message.encode('utf-8'))

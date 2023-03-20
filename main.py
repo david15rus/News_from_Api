2d4cc1ccd3ccbab0e4da57c222e6edfda658ed34
Razial <uck15@bk.ru>
2023-03-20 13:50:40 +0300

    Add links, ass subject, control category

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 import requests
 import os
 from send_email import send_email
-
+topic = 'Россия'
 API_KEY = os.getenv('Api_key')
-url = f'https://newsapi.org/v2/everything?q=russia&' \
-      f'from=2023-02-19&language=ru&' \
-      f'sortBy=publishedAt&apiKey={API_KEY}'
+url = f'https://newsapi.org/v2/everything?' \
+      f'q={topic}&' \
+      f'from=2023-03-19&' \
+      f'language=ru&' \
+      f'sortBy=publishedAt&' \
+      f'apiKey={API_KEY}'
 
 # Make request
 request = requests.get(url)
 
 # Get a dictionary with data
 content = request.json()
-message = ''
+message = "Subject: Новости на сегодня \n"
 
 # Access the article titles and description
 for article in content['articles']:
     message += article['title'] + '\n'
     message += article['description'] + '\n'
-    message += article['source']['name'] + 2*'\n'
+    message += article['url'] + 2*'\n'
 
 send_email(message.encode('utf-8'))
